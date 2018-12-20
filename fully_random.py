edee5b845ffa4c107b167bf063fcd5eccc5161d2
Ivan Koptelov <ivan.koptelov@tarantool.org>
2018-12-20 15:36:24 +0300

    Set right N param for gradient bust

--- fully_random.py
+++ fully_random.py
@@ -7,7 +7,7 @@ from utils import cmp_matrix, print_matrix
 
 
 def prepare_random_data(min_v, max_v):
-    rows = randint(3, 11)
+    rows = randint(3, 7)
     columns = rows
 
     C = [[-1 for c in range(columns)] for r in range(rows)]
@@ -54,22 +54,28 @@ min_v = 100.0
 max_v = 10000.0
 
 
-n_cases = 1
+n_cases = 10
 for case in range(n_cases):
+    print("case {}".format(case))
+
     C, c_to_group, b, G, n_pairs_needed = prepare_random_data(min_v, max_v)
 
     a = 1
     # TODO fix this, it's really strange
     lmbd = [min_v] * len(b)
 
-    N = 100
+    N = 10000
 
     x = normal_solution(C, b, G, a, lmbd, N, c_to_group, n_pairs_needed)
     x_exp = brute_solution(C, b, c_to_group, n_pairs_needed, max_v)
 
     if not cmp_matrix(x, x_exp):
+        print("b")
+        print(*b, sep=' ')
+        print("c_to_group")
+        print(*c_to_group, sep=' ')
         print("attempt number " + str(case) + " broke")
-        print_matrix(C)
+        # print_matrix(C)
         print("expected")
         print_matrix(x_exp)
         print("calculated")
